54463ac6261e9f47db5304ad0863d689d761c68b
Julian Smith <smith.julian.f@gmail.com>
2020-04-19 21:31:03 +0000

    Made modal appear and disappear with button pressed

---
+++ creditsModal.js
@@ -0,0 +1,29 @@
+// .credits
+// #credits-modal
+// #open-credits-modal
+
+var openCreditsModal = document.querySelector('#open-credits-modal');
+var closeCreditsModal = document.querySelector('#close-credits-modal');
+
+var creditsModal = document.querySelector('#credits-modal');
+
+openCreditsModal.addEventListener('click', function () {
+    setCreditsModalVisibility(true)
+});
+
+closeCreditsModal.addEventListener('click', function () {
+    setCreditsModalVisibility(false)
+});
+
+function setCreditsModalVisibility (makeModalVisible) {
+    if(makeModalVisible === true) {
+        creditsModal.style.display = 'block';
+        openCreditsModal.style.display = 'none';
+    }
+    if(makeModalVisible === false) {
+        creditsModal.style.display = 'none';
+        openCreditsModal.style.display = 'block';
+
+    }
+}
+
